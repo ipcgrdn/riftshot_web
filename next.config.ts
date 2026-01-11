68e344787f4752fa19accf7754aed4fe1c8fc420
choi <cejewe@naver.com>
2026-01-11 17:40:06 +0900

    01: 이미지 최적화

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   devIndicators: false,
+
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
 export default nextConfig;
